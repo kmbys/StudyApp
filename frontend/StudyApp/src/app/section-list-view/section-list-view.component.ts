f6db26a555be16635b22aaafa24f4adf945d5091
he23inw3 <ma7k5px@gmail.com>
2020-01-13 14:34:07 +0900

    予定時間と終了時間を追加

--- frontend/StudyApp/src/app/section-list-view/section-list-view.component.ts
+++ frontend/StudyApp/src/app/section-list-view/section-list-view.component.ts
@@ -10,15 +10,21 @@ export class SectionListViewComponent implements OnInit {
   sections = [
     {
       title: 'Java 初級',
-      name: '相良　涼介'
+      name: '相良　涼介',
+      startTime: '14:00',
+      endTime: '15:00',
     },
     {
       title: 'Java 中級',
-      name: '二瓶　由伸'
+      name: '二瓶　由伸',
+      startTime: '15:00',
+      endTime: '16:00',
     },
     {
       title: 'Java 上級',
-      name: '三上　俊'
+      name: '三上　俊',
+      startTime: '17:00',
+      endTime: '18:00',
     }
   ];
 
@@ -26,5 +32,6 @@ export class SectionListViewComponent implements OnInit {
   }
 
   ngOnInit() {
+
   }
 }
