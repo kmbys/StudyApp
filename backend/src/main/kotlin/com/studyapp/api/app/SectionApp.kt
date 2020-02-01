586e9394d299d314a4b238e59fda398d015f00f3
nsbt <55523468+nsbt@users.noreply.github.com>
2020-02-01 17:55:36 +0900

    パス変数取得

--- backend/src/main/kotlin/com/studyapp/api/app/SectionApp.kt
+++ backend/src/main/kotlin/com/studyapp/api/app/SectionApp.kt
@@ -3,6 +3,7 @@ package com.studyapp.api.app
 import com.studyapp.api.service.SectionService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,8 +12,8 @@ class SectionApp {
     @Autowired
     lateinit var service: SectionService
 
-    @GetMapping("studies/000011110001/sections/001")
-    fun get(): SectionResponse {
-        return service.find("000011110001", "001").toResponse()
+    @GetMapping("studies/{studyId}/sections/{sectionId}")
+    fun get(@PathVariable studyId: String, @PathVariable sectionId: String): SectionResponse {
+        return service.find(studyId, sectionId).toResponse()
     }
 }
--- backend/src/main/kotlin/com/studyapp/api/app/StudyApp.kt
+++ backend/src/main/kotlin/com/studyapp/api/app/StudyApp.kt
@@ -3,6 +3,7 @@ package com.studyapp.api.app
 import com.studyapp.api.service.StudyService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,8 +11,8 @@ class StudyApp {
     @Autowired
     lateinit var service: StudyService
 
-    @GetMapping("studies/000011110001")
-    fun get(): StudyResponse {
-        return service.find("000011110001").toResponse()
+    @GetMapping("studies/{id}")
+    fun get(@PathVariable id: String): StudyResponse {
+        return service.find(id).toResponse()
     }
 }
