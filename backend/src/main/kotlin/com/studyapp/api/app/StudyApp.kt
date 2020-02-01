4c988f11b8b19d4bbfd3377e2b3dcd64b7c0e04a
nsbt <55523468+nsbt@users.noreply.github.com>
2020-02-01 09:47:48 +0900

    studyの時刻フォーマット変更

--- backend/src/main/kotlin/com/studyapp/api/app/StudyApp.kt
+++ backend/src/main/kotlin/com/studyapp/api/app/StudyApp.kt
@@ -1,17 +1,17 @@
-package com.studyapp.api
+package com.studyapp.api.app
 
-import com.studyapp.api.app.StudyResponse
-import org.springframework.web.bind.annotation.RestController
+import com.studyapp.api.domain.Study
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
 class StudyApp {
     @GetMapping("studies/000011110001")
     fun get(): StudyResponse {
-        return StudyResponse("Java勉強会",
+        return Study("Java勉強会",
                 LocalDateTime.of(2020, 7, 24, 13, 30),
                 LocalDateTime.of(2020, 7, 24, 18, 0),
-                listOf("001", "002", "003"))
+                listOf("001", "002", "003")).toResponse()
     }
 }
--- backend/src/main/kotlin/com/studyapp/api/app/StudyResponse.kt
+++ backend/src/main/kotlin/com/studyapp/api/app/StudyResponse.kt
@@ -2,4 +2,7 @@ package com.studyapp.api.app
 
 import java.time.LocalDateTime
 
-data class StudyResponse(val name: String, val startTime: LocalDateTime, val endTime: LocalDateTime, val sectionIds: List<String>)
+data class StudyResponse(val name: String,
+                         val startTime: String,
+                         val endTime: String,
+                         val sectionIds: List<String>)
