89f3c706cc71abc634a9d061ad7fb1fdf7cb6031
nsbt <55523468+nsbt@users.noreply.github.com>
2020-02-28 21:26:14 +0900

    cross origin header (temporarily)

--- backend/src/main/kotlin/com/studyapp/api/app/StudyApp.kt
+++ backend/src/main/kotlin/com/studyapp/api/app/StudyApp.kt
@@ -2,6 +2,7 @@ package com.studyapp.api.app
 
 import com.studyapp.api.service.StudyService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +12,7 @@ class StudyApp {
     @Autowired
     lateinit var service: StudyService
 
+    @CrossOrigin
     @GetMapping("studies/{id}")
     fun get(@PathVariable id: String): StudyResponse {
         return service.find(id).toResponse()
